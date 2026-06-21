b279a15dc77a5419efec69f223ce8f67cede3f87
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-06-21 10:47:51 +0700

    Add mobile navigation open state

--- Room homepage/assets/js/main.js
+++ Room homepage/assets/js/main.js
@@ -1,31 +1,31 @@
-const header = document.querySelector(".site-header");
-const hamburgerButton = document.querySelector(".site-header__hamburger");
-const closeButton = document.querySelector(".site-header__close");
-const navLinks = document.querySelectorAll(".site-header__link");
+// const header = document.querySelector(".site-header");
+// const hamburgerButton = document.querySelector(".site-header__hamburger");
+// const closeButton = document.querySelector(".site-header__close");
+// const navLinks = document.querySelectorAll(".site-header__link");
 
-function openNavigation() {
-    header.classList.add("is-open");
-    hamburgerButton.setAttribute("aria-expanded", "true");
-}
+// function openNavigation() {
+//     header.classList.add("is-open");
+//     hamburgerButton.setAttribute("aria-expanded", "true");
+// }
 
-function closeNavigation() {
-    header.classList.remove("is-open");
-    hamburgerButton.setAttribute("aria-expanded", "false");
-}
+// function closeNavigation() {
+//     header.classList.remove("is-open");
+//     hamburgerButton.setAttribute("aria-expanded", "false");
+// }
 
-function toggleNavigation() {
-    const isOpen = header.classList.contains("is-open");
+// function toggleNavigation() {
+//     const isOpen = header.classList.contains("is-open");
 
-    if (isOpen) {
-        closeNavigation();
-    } else {
-        openNavigation();
-    }
-}
+//     if (isOpen) {
+//         closeNavigation();
+//     } else {
+//         openNavigation();
+//     }
+// }
 
-hamburgerButton.addEventListener("click", toggleNavigation);
-closeButton.addEventListener("click", closeNavigation);
+// hamburgerButton.addEventListener("click", toggleNavigation);
+// closeButton.addEventListener("click", closeNavigation);
 
-navLinks.forEach(function (link) {
-    link.addEventListener("click", closeNavigation);
-});
+// navLinks.forEach(function (link) {
+//     link.addEventListener("click", closeNavigation);
+// });
