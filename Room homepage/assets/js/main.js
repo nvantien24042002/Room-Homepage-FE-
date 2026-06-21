c0d13aa4140656bc5c3ee4a285ebe8fd3b00c11c
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-06-21 11:04:19 +0700

    Add mobile navigation toggle

--- Room homepage/assets/js/main.js
+++ Room homepage/assets/js/main.js
@@ -1,31 +1,19 @@
-// const header = document.querySelector(".site-header");
-// const hamburgerButton = document.querySelector(".site-header__hamburger");
-// const closeButton = document.querySelector(".site-header__close");
-// const navLinks = document.querySelectorAll(".site-header__link");
-
-// function openNavigation() {
-//     header.classList.add("is-open");
-//     hamburgerButton.setAttribute("aria-expanded", "true");
-// }
-
-// function closeNavigation() {
-//     header.classList.remove("is-open");
-//     hamburgerButton.setAttribute("aria-expanded", "false");
-// }
-
-// function toggleNavigation() {
-//     const isOpen = header.classList.contains("is-open");
-
-//     if (isOpen) {
-//         closeNavigation();
-//     } else {
-//         openNavigation();
-//     }
-// }
-
-// hamburgerButton.addEventListener("click", toggleNavigation);
-// closeButton.addEventListener("click", closeNavigation);
-
-// navLinks.forEach(function (link) {
-//     link.addEventListener("click", closeNavigation);
-// });
+const header = document.querySelector(".site-header");
+const hamburgerButton = document.querySelector(".site-header__hamburger");
+const closeButton = document.querySelector(".site-header__close");
+const navLinks = document.querySelectorAll(".site-header__link");
+// Event open nav menu
+function openNavigation(){
+    header.classList.add("is-open");
+    hamburgerButton.setAttribute("aria-expanded", "true");
+}
+// Event close nav menu
+function closeNavigation() {
+    header.classList.remove("is-open");
+    hamburgerButton.setAttribute("aria-expanded", "false");
+}
+hamburgerButton.addEventListener("click", openNavigation);
+closeButton.addEventListener("click", closeNavigation);
+navLinks.forEach(function (link) {
+    link.addEventListener("click", closeNavigation);
+});
