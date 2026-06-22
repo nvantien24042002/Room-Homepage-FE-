d3949f3199847f3f336bccce771806e37462895f
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-06-22 17:56:15 +0700

    Add responsive images to about slider

--- Room homepage/assets/js/main.js
+++ Room homepage/assets/js/main.js
@@ -23,38 +23,55 @@ navLinks.forEach(function (link) {
 const aboutImage = document.querySelector(".about__image");
 const aboutTitle = document.querySelector(".about-content__title");
 const aboutText = document.querySelector(".about-content__text");
-
 const previousSlideButton = document.querySelector(".slider__previous");
 const nextSlideButton = document.querySelector(".slider__next");
 
 console.log(aboutImage," - ",aboutTitle," - ",aboutText);
 
 console.log(previousSlideButton," - ",nextSlideButton);
+// desktopMediaQuery biến này kiểm tra responsive
+/*
+    Nếu màn hình >= 48rem
+    -> dùng ảnh desktop
+
+    Nếu màn hình < 48rem
+    -> dùng ảnh mobile
+*/
+const desktopMediaQuery = window.matchMedia("(min-width:768px");
 
 const slides = [
     {
         title: "Discover innovative ways to decorate",
         text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
-        image: "assets/images/desktop-image-hero-1.jpg",
+        image: {
+            mobile: "assets/images/mobile-image-hero-1.jpg",
+            desktop: "assets/images/desktop-image-hero-1.jpg"
+        },
         alt: "Bright living room with modern sofa and decor"
     },
     {
         title: "We are available all across the globe",
         text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
-        image: "assets/images/desktop-image-hero-2.jpg",
+        image: {
+            mobile: "assets/images/mobile-image-hero-2.jpg",
+            desktop: "assets/images/desktop-image-hero-2.jpg"
+        },
         alt: "Three stylish chairs arranged in a clean interior"
     },
     {
         title: "Manufactured with the best materials",
         text: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
-        image: "assets/images/desktop-image-hero-3.jpg",
+        image: {
+            mobile: "assets/images/mobile-image-hero-3.jpg",
+            desktop: "assets/images/desktop-image-hero-3.jpg"
+        },
         alt: "Minimalist black chair with soft lighting"
     }
 ];
 let currentSlideIndex = 0; //Đang ở slider dầu tiên
 function renderSlide() {
     const currentSlide = slides[currentSlideIndex]; //Lấy slider hiện tại
-    aboutImage.src = currentSlide.image; // Đổi ảnh
+    aboutImage.src = getSlideImage(currentSlide); // Đổi ảnh
     aboutImage.alt = currentSlide.alt; // Đổi ALt
     aboutTitle.textContent = currentSlide.title; //Đổi title của ảnh
     aboutText.textContent = currentSlide.text; //Đổi paragraph của ảnh
@@ -74,5 +91,15 @@ function goPreviousSlide() {
     currentSlideIndex--;
     renderSlide();
 }
+// function get images responsive screen
+function getSlideImage(slide){
+    // Check which screen you are currently on to render the image correctly for that screen.
+    if (desktopMediaQuery.matches) {
+        return slide.image.desktop;
+    }
+    return slide.image.mobile
+}
 nextSlideButton.addEventListener("click", goNextSlide);
 previousSlideButton.addEventListener("click", goPreviousSlide);
+desktopMediaQuery.addEventListener("change",renderSlide);
+renderSlide();
